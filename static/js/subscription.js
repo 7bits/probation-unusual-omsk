eeb92c7af3efd2e433c3e68382d5d201df7c9df7
alex <alex@alex-VirtualBox.(none)>
2013-08-14 12:26:12 +0700

    modified subscription

---
+++ static/js/subscription.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#subscription-form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "subscribe/",
+            type: "POST",
+            dataType: "json",
+            data: $(this).serialize(), 
+            success: function(data) {
+                // Удаляем ошибки если были
+                //$form.find('.error').remove();
+                if (data['result'] == 'success') {
+                    // Делаем что-то полезное
+                    $(id_email).val("");
+                    alert("Спасибо за подписку!");
+                }
+                else if (data['result'] == 'error') {
+                    // Показываем ошибки
+                    //$form.replaceWith(data['response']);
+                    alert("не правильный e-mail!");
+                }
+            },
+            error: function(){
+                alert("failure");
+            },
+        });
+    });
+});
