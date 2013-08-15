8dc9e49fdfc789075e4a2cb51c55f6875aa54800
alex <alex@SamuraiU.(none)>
2013-08-16 01:57:31 +0700

    add filter

--- street_faces/core/views.py
+++ street_faces/core/views.py
@@ -42,3 +42,9 @@ def places_map(request):
     return render(request, 'map.html', {
         'all_places': all_places,
         'subscription_form': subscription_form})
+
+def places_filter(request, filter_id):
+    one_place = get_object_or_404(place, category=filter_id)
+    return render(request, 'place.html', {
+        'place': one_place,
+        'subscription_form': subscription_form})
