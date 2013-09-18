d25fd0019c673fd7b32b31c75864df5188ec95ed
alex <alex@alex-VirtualBox.(none)>
2013-09-18 15:55:13 +0700

    add middleware for subscription

--- street_faces/subscription/proxy.py
+++ street_faces/subscription/proxy.py
@@ -1,14 +1,6 @@
 from street_faces.subscription.forms import subscription_form
-from django.template import RequestContext
-from  django.http import HttpResponse
-from django.shortcuts import render
 
 
 class form_middleware(object):
     def process_request(self, request):
     	request.subscription_form = subscription_form
-        #response = HttpResponse(response, {'subscription_form': subscription_form})
-        #response['subscription_form'] = subscription_form
-        #response.context += RequestContext(response, {'subscription_form': subscription_form})
-        #response.content = response.content.replace({'subscription_form': subscription_form}) 
-        #return response
