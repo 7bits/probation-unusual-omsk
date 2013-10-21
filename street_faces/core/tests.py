84461c6df19eecbefe60b2a57148c48d8d9edf15
alex <alex@alex-VirtualBox.(none)>
2013-10-21 18:16:56 +0700

    add tests

--- street_faces/core/tests.py
+++ street_faces/core/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,11 +7,49 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def input_login(self, user_name, user_pass):
+    self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+    username_input = self.selenium.find_element_by_name("username")
+    username_input.send_keys(user_name)
+    password_input = self.selenium.find_element_by_name("password")
+    password_input.send_keys(user_pass)
+    self.selenium.find_element_by_name("login").click()
+
+
+class login_test(LiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(login_test, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(login_test, cls).tearDownClass()
+
+    def test_login_ok(self):
+        user_name = "test2"
+        user_pass = "test2"
+        new_user = User.objects.create_user(user_name, None, user_pass)
+        input_login(self, user_name, user_pass)
+        WebDriverWait(self.selenium, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.CLASS_NAME, "title-index")))
+
+    def test_login_failed(self):
+        user_name = ""
+        user_pass = ""
+        input_login(self, user_name, user_pass)
+        WebDriverWait(self.selenium, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.CLASS_NAME, "error-login")))
--- street_faces/subscription/tests.py
+++ street_faces/subscription/tests.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,11 +7,43 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def input_subscription (self, user_email):
+        self.selenium.get('%s%s' % (self.live_server_url, '/'))
+        email_input = self.selenium.find_element_by_name("email")
+        email_input.send_keys(user_email)
+        self.selenium.find_element_by_id("subscription-button").click()
+        alert = self.selenium.switch_to_alert()
+        alert_text = alert.text
+        alert.accept()
+        return alert_text
+    
+class test_subsription(LiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(test_subsription, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(test_subsription, cls).tearDownClass()
+
+    def test_subsription_ok(self):
+        user_email = "test2@mail.ru"
+        alert_text = input_subscription(self, user_email)
+        self.assertEqual(alert_text, u"Спасибо за подписку!")
+        
+    def test_subsription_failed(self):
+        user_email = "test2mail.ru"
+        alert_text = input_subscription(self, user_email)
+        self.assertEqual(alert_text, u"Неправильный e-mail!")
